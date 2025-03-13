bdde5b3f1a1e53de10ac3799404a0f5ee88ec992
Racheal Späni <37956115+xomen85@users.noreply.github.com>
2025-03-13 12:16:23 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const searchBarContainerEl = document.querySelector
+(".search-bar-container")
+
+const magnifierEl = document.querySelector(".magnifier")
+
+magnifierEl.addEventListener("click",()=>{
+    searchBarContainerEl.classList.toggle("active")
+   
+})
